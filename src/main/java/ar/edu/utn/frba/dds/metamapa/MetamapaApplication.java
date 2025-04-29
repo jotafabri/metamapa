0ce6ff7e51fd94efe573ee24f5cd38b9b7871cc4
Josué Fabrizio De Simone <jdesimone@frba.utn.edu.ar>
2025-04-29 19:52:24 -0300

    Agregamos la dependencia de spring

---
+++ src/main/java/ar/edu/utn/frba/dds/metamapa/MetamapaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.metamapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetamapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetamapaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frba/dds/metamapa/MetamapaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.dds.metamapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetamapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
