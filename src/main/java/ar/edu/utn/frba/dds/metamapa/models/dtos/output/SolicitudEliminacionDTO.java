8ab05876a9fbf3ba7da4493af44eafd371879b33
Francisco <fdeclich1@gmail.com>
2025-04-30 12:53:51 -0300

    Le pongo el import

--- src/main/java/ar/edu/utn/frba/dds/metamapa/models/dtos/output/SolicitudEliminacionDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/metamapa/models/dtos/output/SolicitudEliminacionDTO.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.dds.metamapa.models.dtos.output;
 
 import ar.edu.utn.frba.dds.metamapa.models.entities.Hecho;
+import lombok.Data;
 
 @Data
 public class SolicitudEliminacionDTO {
