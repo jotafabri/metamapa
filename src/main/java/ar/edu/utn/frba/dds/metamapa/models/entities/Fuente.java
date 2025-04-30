7981bc07e21153b47642b0cb6ac3f90372427040
Josué Fabrizio De Simone <jdesimone@frba.utn.edu.ar>
2025-04-30 12:07:40 -0300

    Changes by Francisco

--- src/main/java/ar/edu/utn/frba/dds/metamapa/models/entities/Fuente.java
+++ src/main/java/ar/edu/utn/frba/dds/metamapa/models/entities/Fuente.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frba.dds.metamapa.models.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public abstract class Fuente {
   protected List<Hecho> listaHechos = new ArrayList<>();
@@ -13,6 +14,12 @@ public abstract class Fuente {
   public Hecho buscarHechoPorTitulo(String titulo) {
     //  filter + findAny
     return listaHechos.stream().filter(h -> titulo.equals(h.getTitulo())).findFirst().orElse(null);
+  }
+
+  public void agregarHecho(Hecho hecho) {
+    Optional<Hecho> hecho2 = listaHechos.stream().filter(h -> h.getTitulo().equalsIgnoreCase(hecho.getTitulo())).findFirst();
+    hecho2.ifPresentOrElse(h ->
+      h.actualizarHecho(hecho), () -> listaHechos.add(hecho));
   }
 
   public void etiquetarPorTitulo(String titulo, String etiqueta) {
