5e9df7dc43aa3022c0845e0f2de02def6758ab11
Josué Fabrizio De Simone <jdesimone@frba.utn.edu.ar>
2025-09-07 23:26:27 -0300

    Diagrama y eliminar un importe sin utilizar

--- src/test/java/ar/edu/utn/frba/dds/metamapa/AggregationServiceTest.java
+++ src/test/java/ar/edu/utn/frba/dds/metamapa/AggregationServiceTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
