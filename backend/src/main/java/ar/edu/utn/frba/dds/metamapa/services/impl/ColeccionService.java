9785e16f8ebb62e6ea2bcd9aba3764550fdf2b89
Germán Vautier <gvautier@frba.utn.edu.ar>
2025-11-04 23:18:05 -0300

    15.-Ya se pueden eliminar colecciones. El problema era que el metodo de eliminar coleccion necesitaba la anotacion @Transactional , en general los metodos que modifican cosas en la base de datos necesitan esta anotacion. TODO:TENER EN CUENTA PARA LOS DEMAS METODOS!

--- backend/src/main/java/ar/edu/utn/frba/dds/metamapa/services/impl/ColeccionService.java
+++ backend/src/main/java/ar/edu/utn/frba/dds/metamapa/services/impl/ColeccionService.java
@@ -19,6 +19,7 @@ import ar.edu.utn.frba.dds.metamapa.models.entities.enums.TipoAlgoritmo;
 import ar.edu.utn.frba.dds.metamapa.models.entities.hechos.Coleccion;
 import ar.edu.utn.frba.dds.metamapa.models.repositories.IColeccionesRepository;
 import ar.edu.utn.frba.dds.metamapa.services.IColeccionService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -97,6 +98,7 @@ public class ColeccionService implements IColeccionService {
 
   //ADMIN:Operacion (C)RUD
   @Override
+  @Transactional
   public ColeccionDTO crearDesdeDTO(ColeccionDTO coleccionDTO) {
     Coleccion coleccion = new Coleccion(coleccionDTO.getTitulo(), coleccionDTO.getDescripcion());
     coleccion.setHandle(generarHandleUnico(coleccion.getTitulo()));
@@ -113,6 +115,7 @@ public class ColeccionService implements IColeccionService {
 
   //ADMIN:Operacion CR(U)D
   @Override
+  @Transactional
   public ColeccionDTO actualizarColeccion(String handle, ColeccionDTO coleccionDTO) {
     Coleccion coleccion = intentarRecuperarColeccion(handle);
 
@@ -138,6 +141,7 @@ public class ColeccionService implements IColeccionService {
 
   //ADMIN:Operacion CRU(D)
   @Override
+  @Transactional
   public void eliminarColeccion(String handle) {
     Coleccion coleccion = intentarRecuperarColeccion(handle);
 
